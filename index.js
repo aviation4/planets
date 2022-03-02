429b7e6f248d2d2296da50e5766ce622470817ee
aviation4 <przemek.kaminski4@gmail.com>
2022-03-02 11:21:51 +0100

    Logic for hamburger menu

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const hamburgerButton = document.getElementById("hamburger__button");
 const hamburgerMenu = document.getElementById("hamburger__menu");
+const main = document.getElementById("main");
+const secondaryNav = document.getElementById("secondaryNav");
+
 
 const toggleHamburgerMenu = () => {
 
   if (hamburgerButton.classList.contains("hamburger__buttonClass--enabled")){
     hamburgerButton.classList.remove("hamburger__buttonClass--enabled");
     hamburgerMenu.style.display = "none";
+    main.style.display = "block";
+    secondaryNav.style.display = "flex";
   } else {
     hamburgerButton.classList.add("hamburger__buttonClass--enabled");
     hamburgerMenu.style.display = "block";
+    main.style.display = "none";
+    secondaryNav.style.display = "none";
   }
 
 }
